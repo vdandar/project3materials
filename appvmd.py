24f2e03a46974bc152774159d84059b0fe3297c9
vdandar <71355515+vdandar@users.noreply.github.com>
2021-03-17 17:39:25 -0400

    Update appvmd.py

--- appvmd.py
+++ appvmd.py
@@ -10,16 +10,6 @@ from flask import Flask, request, render_template, jsonify
 import pymysql
 pymysql.install_as_MySQLdb()
 
-x_rapidapi_key = "651aaa93a5msh16007951fa19705p181cc7jsnf87c2b4a7e46"
-x_rapidapi_host = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-
-spoonacular_API = "19d2608eee594d3797334b2d6370385f"
-    
-remote_db_endpoint = "databootcamp.cv1u8ipt6nbb.us-east-2.rds.amazonaws.com"
-remote_db_port = "3306"
-remote_db_name = "recipes"
-remote_db_user = "admin"
-remote_db_pwd = "postgres123"
 
 def recommendations(recipe_ids_list = [1554861,1560677,1571559]):
     
